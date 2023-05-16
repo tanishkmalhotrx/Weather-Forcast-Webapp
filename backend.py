cc889259a49d046a0ede0a04baa4a84a19a95d30
Tanishk Malhotra <80147670+tanishkmalhotrx@users.noreply.github.com>
2023-05-16 12:21:31 +0530

    added sky section

--- backend.py
+++ backend.py
@@ -2,17 +2,13 @@ import requests
 
 API_KEY = "5eb86ef4d90c936f346fc2470d52981f"
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 if __name__ == "__main__":
